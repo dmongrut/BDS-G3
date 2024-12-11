a12a557bac075ede0b55442feeeb677432967047
David <david.mongrut@umch.edu.pe>
2024-12-11 17:41:30 -0500

    crud

---
+++ Modulo1/dia3/05-crud.py
@@ -0,0 +1,70 @@
+import os
+from time import sleep
+
+"""
+CREA
+  -CREATE
+  -READ
+  -UPDATE
+  -DELETE
+"""
+dic_alumnos = {
+  "12345678":{
+      "nombre": "CESAR",
+      "email": "cesar@gmail.com"
+  }
+}
+
+ANCHO = 50
+opcion = 0
+
+while(opcion < 5):
+    os.system("clear")
+
+    print("=" * ANCHO)
+    print(" " * 10 + "GESTION DE ALUMNOS")
+    print("=" * ANCHO)
+    print("""
+          [1] REGISTRAR ALUMNO
+          [2] MOSTRARR ALUMNO
+          [3] ACTUALIZAR ALUMNO
+          [4] ELIMINAR ALUMNO
+          [5] SALIR
+          """)
+    print("=" * ANCHO)
+    opcion = int(input("INGRESE OPCION: "))
+
+    os.system("clear")
+
+    if opcion == 1:
+           print("=" * ANCHO)
+           print(" " * 10 + "[1] REGISTRAR ALUMNO")
+           print("=" * ANCHO)
+
+    elif opcion == 2:
+          print("=" * ANCHO)
+          print(" " * 10 + "[2] MOSTRAR ALUMNO")
+          print("=" * ANCHO)
+    
+    elif opcion == 3:
+          print("=" * ANCHO)
+          print(" " * 10 + "[3] ACTUALIZAR ALUMNO")
+          print("=" * ANCHO)
+
+    elif opcion == 4:
+          print("=" * ANCHO)
+          print(" " * 10 + "[4] ELIMINAR ALUMNO")
+          print("=" * ANCHO)
+
+    elif opcion == 5:
+          print("=" * ANCHO)
+          print(" " * 10 + "[5] SALIR DEL PROGRAMA")
+          print("=" * ANCHO)
+    
+    else:
+          print("=" * ANCHO)
+          print(" " * 10 + "OPCION INVALIDA!!!")
+          print("=" * ANCHO)
+
+    sleep(1)
+
